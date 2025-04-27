107e7c810213958267f1f9088ecee2ebd0567d2e
infinity1984-infi <ind.agamer7@gmail.com>
2025-04-27 10:20:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Your BotFather token
+API_TOKEN = "123456789:ABCdefGhIjKlmNoPQRsTuvWX"
+
+# The ID of your special "batch" or "registration" channel
+# (where you'll forward posts so the bot can learn the origin chat IDs)
+REGISTRATION_CHAT_ID = -1001234567890
